e153ddba77131b71f0fdac1b3b78d2d61a950856
Paweł Fabrycki <pawel.fabrycki@infotech.edu.pl>
2025-02-18 13:41:35 +0100

    feat: Updated 66

--- 66/main.py
+++ 66/main.py
@@ -4,15 +4,17 @@ def sumaCyfr(liczba):
         suma += int(cyfra)
     return suma
 
-def isPrime(liczba): # liczba jest typu int
+def first(liczba): # liczba jest typu int
     if liczba < 2:
         return False
     elif liczba == 2:
         return True
     elif liczba % 2 == 0:
         return False
-    else:
-        return True
+    for i in range(3, int(liczba**0.5), 2):
+        if liczba % i == 0:
+            return False
+    return True
 
 
 
@@ -25,3 +27,8 @@ print(listaTrojek)
 for liczby in listaTrojek:
     if sumaCyfr(liczby[0]) + sumaCyfr(liczby[1]) == int(liczby[2]):
         print(liczby)
+
+for liczby in listaTrojek:
+    if int(liczby[0]) * int(liczby[1]) == int(liczby[2]):
+        if first(int(liczby[0])) and first(int(liczby[1])):
+            print(liczby)
